4f70e86ac629d25b9f7c934c5ba05254ade57782
Alindato Leonard <alindatoleonard619@gmail.com>
2025-11-05 16:37:28 +0000

    chnage this picture to flying fish"https://cdn.bestsuppliers.com/seo_pro

--- next.config.ts
+++ next.config.ts
@@ -88,6 +88,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.bestsuppliers.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
