129d1e440a5c1dba8ad146a836139a3111e6b2f4
Alindato Leonard <alindatoleonard619@gmail.com>
2025-11-05 15:09:58 +0000

    put this in Banana boiat picture"https://tse2.mm.bing.net/th/id/OIP.4yvW

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'tse2.mm.bing.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
