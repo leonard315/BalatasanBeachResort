7d6e39feb966dacd41111e22f9f2a9e5f04f917a
Alindato Leonard <alindatoleonard619@gmail.com>
2025-11-05 14:51:20 +0000

    can you put this to 3 floating cottage"https://tse1.mm.bing.net/th/id/OI

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'tse1.mm.bing.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
